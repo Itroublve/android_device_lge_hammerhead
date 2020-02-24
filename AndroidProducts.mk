7acc2204e1227253bc1559790d7256c4bd2c7572
Itroublve <51215044+Itroublve@users.noreply.github.com>
2020-02-24 16:40:55 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_hammerhead.mk
+    $(LOCAL_DIR)/carbon_hammerhead.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_hammerhead-user \
-    lineage_hammerhead-userdebug \
-    lineage_hammerhead-eng
+    carbon_hammerhead-user \
+    carbon_hammerhead-userdebug \
+    carbon_hammerhead-eng
